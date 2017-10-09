0ea32b046ebf5542b2795eb92642f1e5e488bbd8
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2017-10-09 16:48:24 +0200

    Zdrojaky z druheho cviceni.

--- cv02/src/cv02/Cv02.java
+++ cv02/src/cv02/Cv02.java
@@ -3,29 +3,37 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cv01;
+package cv02;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import static java.lang.Math.abs;
+import static java.lang.Math.abs;
 
 /**
  *
  * @author jethro
  */
-public class Cv01 {
+public class Cv02 {
 
     /**
      * @param args the command line arguments
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-        System.out.println("Test");
-        int a;
-        a  = readInt();
-        System.out.println(a+2);
-        System.out.println(readDouble()*2);
+        System.out.print("Zadej uhel:");
+        double a = readDouble();
+        int d;
+        d = (int)a;
+        int m = abs((int) ((a-d)*60));
+        double s = abs((a-d) * 3600) - m * 60;
+        System.out.format("Je to %d stupnu, %d minut a %12.2f vterin\n", d, m, s);
+        if (d < 0){
+            System.out.println("Stupne byly zaporne");
+        } else {
+            System.out.println("Stupne byly nezaporne");
+        }
     }
     
     public static int readInt() throws IOException{
