111edddc54a4a53948bab5d83822c63f3f917076
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2017-10-24 10:55:15 +0200

    Zdrojove kody z ctvrteho cviceni.

--- cv04-1/src/cv04/pkg1/Cv041.java
+++ cv04-1/src/cv04/pkg1/Cv041.java
@@ -8,6 +8,7 @@ package cv04.pkg1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import static java.lang.Math.sqrt;
 
 /**
  *
@@ -19,7 +20,80 @@ public class Cv041 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Ukazka nepresnosti floatu ve velkych cislech
+        float f;
+        f= 100000000;
+        if (f+1 == f){
+        System.out.println("Rovna se");
+        } else {
+        System.out.println("Nerovna se");
+        }
+        
+        // Vypocet vzdalenosti
+        double d;
+        d = dist(10,10,20,20);
+        System.out.println(d);
+        System.out.println(dist(10,10,13,14));
+        
+        quadEqPrint(4,4,1);
+        
+        double res[];
+        //res = quadEq(4,4,1);
+        //res = quadEq(1,0,1);
+        res = quadEq(4,-5,1);
+
+        System.out.format("Rovnice má %d řešení\n",res.length);
+        if (res.length >= 1){
+            System.out.println(res[0]);
+        }
+        if (res.length == 2){
+            System.out.println(res[1]);
+        }
+        
+    }
+    
+    public static void quadEqPrint(double a, double b, double c){
+        double d;
+        d = b*b - 4*a*c;
+        if (d<0){
+            System.out.println("Nemá řešení");
+        } else if (d==0){
+            System.out.println("Má 1 řešení");
+            System.out.println((-b+sqrt(d))/(2*a));
+        } else {
+            System.out.println("Má dvě řešení");
+            System.out.println((-b+sqrt(d))/(2*a));
+            System.out.println((-b-sqrt(d))/(2*a));
+
+        }
+    }
+        public static double[] quadEq(double a, double b, double c){
+        double d;
+        d = b*b - 4*a*c;
+        if (d<0){
+            return new double[0];
+        } else if (d==0){
+            double res[];
+            res = new double[1];
+            res[0] = (-b+sqrt(d))/(2*a);
+            return res;
+        } else {
+            double res[];
+            res = new double[2];
+            res[0] = (-b+sqrt(d))/(2*a);
+            res[1] = (-b-sqrt(d))/(2*a);
+            return res;
+        }
+    }
+    
+    public static double dist(double ax, double ay, double bx, double by){
+        double dx;
+        double dy;
+        dx = bx-ax;
+        dy = by-ay;
+        double d;
+        d = sqrt(dx*dx + dy*dy);
+        return d;
     }
     public static int readInt() throws IOException{
         BufferedReader reader;
